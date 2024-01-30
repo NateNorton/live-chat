cb1abe1402c3e7b148ba3b1616e942574a4e7d02
Nathan <nathangnorton@gmail.com>
2024-01-30 12:10:28 +0100

    login implemented

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,21 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyB8RU1Fs3HfVI0P_3Kt1rz9wowxsPE7U2o',
-    authDomain: 'learning-vue-60a9e.firebaseapp.com',
-    projectId: 'learning-vue-60a9e',
-    storageBucket: 'learning-vue-60a9e.appspot.com',
-    messagingSenderId: '218142734759',
-    appId: '1:218142734759:web:63289f820c9fcd4b289e82',
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
 };
 
 // init firebase
 firebase.initializeApp(firebaseConfig);
 
+const projectAuth = firebase.auth();
 const projectFirestore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectFirestore, timestamp };
+export { projectFirestore, timestamp, projectAuth };
