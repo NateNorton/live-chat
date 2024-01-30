4df53a3c66abebbbdd14447f31728ef2f2deca97
Nathan <nathangnorton@gmail.com>
2024-01-30 13:54:05 +0100

    add protected route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Welcome from '@/views/Welcome.vue';
 import Chatroom from '@/views/Chatroom.vue';
+import { projectAuth } from '../firebase/config';
+
+// auth guard
+const requireAuth = (to, from, next) => {
+    let user = projectAuth.currentUser;
+    if (!user) {
+        next({ name: 'Welcome' });
+    } else {
+        next();
+    }
+};
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +25,7 @@ const router = createRouter({
             path: '/chatroom',
             name: 'Chatroom',
             component: Chatroom,
+            beforeEnter: requireAuth,
         },
     ],
 });
